c3df625d8c68e4bd01b866c60a0d56edf68eda4f
dragon-123 <1106302861@qq.com>
2018-09-09 10:51:31 +0800

    â之ä创建一个springboot项目

---
+++ src/main/java/ei/aliyun/controller/HelloController.java
@@ -0,0 +1,18 @@
+package ei.aliyun.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        System.out.println("==========");
+        return "ok";
+    }
+}
