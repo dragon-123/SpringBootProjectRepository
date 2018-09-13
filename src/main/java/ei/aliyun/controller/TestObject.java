f145dd801d31a86c9ebc1ec92fae02b349caf74e
dragon-123 <1106302861@qq.com>
2018-09-13 21:03:37 +0800

    bug fix

--- src/main/java/ei/aliyun/controller/TestObject.java
+++ src/main/java/ei/aliyun/controller/TestObject.java
@@ -3,5 +3,6 @@ package ei.aliyun.controller;
 public class TestObject {
     public static void main(String[] args) {
         System.out.println("this is dev");
+        System.out.println("bug fix");
     }
 }
